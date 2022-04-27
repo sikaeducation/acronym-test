4b7b8a3076114e3464df3872798ce6a7fb7d54c1
Kyle Coberly <kyle.coberly@gmail.com>
2022-04-27 17:49:51 -0600

    Add solution

--- acronym.ts
+++ acronym.ts
@@ -1,1 +1,12 @@
-export function parse() {}
+export function parse(phrase: string): string {
+  if (typeof phrase !== "undefined" && phrase !== undefined) {
+    const match = phrase.match(/[A-Z]+[a-z]*|[a-z]+/g);
+    return !match
+      ? ""
+      : match.reduce(
+          (acronym: string, word: string) => (acronym += word[0].toUpperCase()),
+          ""
+        );
+  }
+  return "";
+}
